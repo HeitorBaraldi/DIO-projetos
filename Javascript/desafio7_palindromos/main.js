b44e82e665d00ee3b0605affd63988fe15c780cb
HeitorBaraldi <carlos.hgb88@gmail.com>
2022-11-11 10:55:14 -0300

    palindromo

---
+++ Javascript/desafio7_palindromos/main.js
@@ -0,0 +1,11 @@
+let resultado = "digital"
+
+function reverseString(resultado){
+   const newString = resultado.split("").reverse().join("")
+   
+   if(newString == resultado){
+      console.log("TRUE")
+   }else{console.log("FALSE")}
+
+}
+reverseString(resultado)
